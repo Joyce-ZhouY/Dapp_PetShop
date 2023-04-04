a99fbbfcd7adb1baae9fe951cea7aafbde8cb96a
Yang <59710320+Joyce-ZhouY@users.noreply.github.com>
2023-04-04 17:10:55 -0400

    update test

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -9,12 +9,29 @@ contract TestAdoption {
  Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
 // The id of the pet that will be used for testing
- uint expectedPetId = 8;
+ uint expectedPetId = 0;
 
 //The expected owner of adopted pet is this contract
  address expectedAdopter = address(this);
 
-// Testing the adopt() function
+ // Number of pets
+ uint public petsNum;
+
+// run before every test
+ function befereTest() public{
+  petsNum = 0;
+ }
+
+
+
+// Tetsing adding a new pet
+function testAddANewPet() public{
+  uint id = adoption.add("Jack", "https://ipfs.io/ipfs/QmY3MKgdpngKTQwdKHmwCdA6Mb9ouS7HbByvQJfuFnRZMz", 2, "Panda", "China");
+  Assert.equal(petsNum, 1, "Number of pets should be one");
+}
+
+
+  // Testing the adopt() function
 function testUserCanAdoptPet() public {
   uint returnedId = adoption.adopt(expectedPetId);
 
@@ -37,5 +54,19 @@ function testGetAdopterAddressByPetIdInArray() public {
 }
 
 
+// Testing retrieval of a pet's details 
+function testGetPetDetailByPetId() public {
+  (uint id,string memory name,string memory picture, uint age,string memory breed,string memory loaction,address payable adoptor,uint date, bool isAdopted) = adoption.getPetDetails(expectedPetId);
+  Assert.equal(id, expectedPetId, "Wrong id");
+  Assert.equal(name, "Jack", "Wrong name");
+  Assert.equal(picture, "https://ipfs.io/ipfs/QmY3MKgdpngKTQwdKHmwCdA6Mb9ouS7HbByvQJfuFnRZMz", "Wrong picture");
+  Assert.equal(age, 2, "Wrong age");
+  Assert.equal(breed, "Panda", "Wrong breed");
+  Assert.equal(loaction, "China", "Wrong location");
+  Assert.equal(adoptor, expectedAdopter, "Wrong adopter");
+  Assert.equal(isAdopted, true, "The pet was adopted");
 }
+  
+}
+
 
